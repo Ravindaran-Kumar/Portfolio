d4e80b4026b88689ca743e9c2c2746a31daf1454
Ravindaran-Kumar <160477228+Ravindaran-Kumar@users.noreply.github.com>
2024-02-19 19:13:59 +0530

    Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,1 +1,101 @@
+var avindaranText1 = document.getElementById("avindaranText1");
+      if (avindaranText1) {
+        avindaranText1.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='headerContainer']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        });
+      }
+      
+      var homeText1 = document.getElementById("homeText1");
+      if (homeText1) {
+        homeText1.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='headerContainer']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        });
+      }
+      
+      var frameAboutContainer1 = document.getElementById("frameAboutContainer1");
+      if (frameAboutContainer1) {
+        frameAboutContainer1.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='about']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        });
+      }
 
+      var frameAboutContainer2 = document.getElementById("frameAboutContainer2");
+      if (frameAboutContainer2) {
+        frameAboutContainer2.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='services']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        });
+      }
+
+      
+      
+      var frameAboutContainer3 = document.getElementById("frameAboutContainer3");
+      if (frameAboutContainer3) {
+        frameAboutContainer3.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='projectsContainer']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        });
+      }
+      
+      var avindaranText = document.getElementById("avindaranText");
+      if (avindaranText) {
+        avindaranText.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='headerContainer']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        });
+      }
+      
+      var homeText = document.getElementById("homeText");
+      if (homeText) {
+        homeText.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='headerContainer']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        });
+      }
+      
+      var aboutText = document.getElementById("aboutText");
+      if (aboutText) {
+        aboutText.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='about']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        });
+      }
+      
+      var servicesText = document.getElementById("servicesText");
+      if (servicesText) {
+        servicesText.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='services']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        });
+      }
+
+      var textButtonContainer = document.getElementById("textButtonContainer");
+      if (textButtonContainer) {
+        textButtonContainer.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='projectsContainer']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        });
+      }
